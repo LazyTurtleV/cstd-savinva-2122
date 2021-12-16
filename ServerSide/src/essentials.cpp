8d0e4e56549daac8181fb85b73d9bbd59693f848
VladymyrAlekseevich <SavinVolodya001@gmail.com>
2021-12-16 23:43:04 +0200

    refactor: move game session loop into another file

---
+++ ServerSide/src/essentials.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+
+#include "../headers/kernel.h"
+#include "../headers/input.h"
+
+void game_session()
+{   
+    init_game();
+
+    while(1)
+    {
+        char *in = receive_input();
+        if(in == WAIT_FOR_INPUT) {
+            Serial.println("oh...");
+            return;
+        }
+
+        Serial.print("Input received: ");
+        Serial.println(in);
+
+        char move_stat = make_move(in[0], in[1]);
+        Serial.print(move_stat);
+
+        char game_stat = game_status();
+        Serial.print(game_stat);
+        if (game_stat != GAME_CONTINUES)
+        {
+            announce_winner();
+            break;
+        }
+    }
+
+    end_game();
+}
--- ServerSide/src/main.cpp
+++ ServerSide/src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 
-#include "../headers/kernel.h"
-#include "../headers/input.h"
+#include "../headers/essentials.h"
 
 void setup()
 {   
@@ -9,23 +8,10 @@ void setup()
 
     //until connection with serial is established
     while(!Serial);
-
-    init_game();
 }
 
 void loop()
 {   
-    char *in = receive_input();
-    if(in == WAIT_FOR_INPUT) return;
-
-    char move_stat = make_move(in[0], in[1]);
-    Serial.print(move_stat);
-
-    char game_stat = game_status();
-    Serial.print(game_stat);
-    if (game_stat != GAME_CONTINUES)
-    {
-        announce_winner();
-        end_game();
-    }
+    //Serial.println("Hello world!!!");
+    game_session();
 }
