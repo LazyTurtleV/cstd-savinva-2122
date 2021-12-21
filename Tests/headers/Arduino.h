d6b44bfc76770f91f3e0dd20c4ba19d4810443d4
VladymyrAlekseevich <SavinVolodya001@gmail.com>
2021-12-21 23:27:30 +0200

    upd: Serial mock

--- Tests/headers/Arduino.h
+++ Tests/headers/Arduino.h
@@ -7,55 +7,53 @@ class SerialClass
 public:
     static void begin(int _n)
     {
-        puts("Begin...");
     }
 
     static int read()
     {
-        puts("Reading");
-        return 100;
+        return 1;
     }
 
     static int print(char *_m)
     {
-        puts("Printing");
+        puts(_m);
         return 100;
     }
 
     static int print(char _m)
     {
-        puts("Printing");
-        return 100;
+        printf("%c", _m);
+        return 1;
     }
 
     static int println(char *_m)
     {
-        puts("Printing in a new line");
+        puts(_m);
         return 100;
     }
 
     static int write(char *_b, size_t _n)
     {
-        puts("Writing");
-        return 100;
+        puts(_b);
+        return _n;
     }
 
     static int write(const uint8_t *_b, size_t _n)
     {
-        puts("Writing");
+        puts((const char*)_b);
         return 100;
     }
 
     static int write(char *_b)
     {
-        puts("Writing");
+        puts(_b);
         return 100;
     }
 
     static int write(char _b)
     {
-        puts("Writing");
-        return 100;
+        printf("%c", _b);
+        return 1;
     }
 
     static bool available()
